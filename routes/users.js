53f0518eaa903c58e7284e6d8d1863e51106b143
bpaw <bpaw.dev@gmail.com>
2017-10-27 18:20:05 -0700

    working example using the routes directory

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/other_me', function(req, res) {
+	var other_me = {
+		"name" : "other brandon"
+	};
+	res.json(other_me);
+});
+
 module.exports = router;
